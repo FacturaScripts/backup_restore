7441119573c49aa824807415f57842363dd52179
Rafael Salas <rsalas.match@gmail.com>
2017-01-04 16:09:52 +0100

    Se guarda el comando entre comillas para evitar error en windows

--- controller/backup_restore.php
+++ controller/backup_restore.php
@@ -177,7 +177,7 @@ class backup_restore extends fs_controller {
             $lanza_comando = '"'."$cmd". '"' . " --version";
             exec($lanza_comando, $resultado);
             if (!empty($resultado[0])) {
-               return $cmd;
+               return $lanza_comando;
             }
          }
       }
