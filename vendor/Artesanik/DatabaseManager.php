106f4a9c367e832b19cc44b8dd06daeb9d11c9e3
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2017-01-06 14:29:31 +0100

    Estilo de codigo con el autoformato de NetBeans

--- vendor/Artesanik/DatabaseManager.php
+++ vendor/Artesanik/DatabaseManager.php
@@ -17,85 +17,90 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace Artesanik;
 
 use Artesanik\DBProcess\MysqlProcess;
 use Artesanik\DBProcess\PostgresqlProcess;
+
 /**
  * Description of DatabaseManager
  *
  * @author Joe Nilson <joenilson at gmail.com>
  */
 class DatabaseManager {
-    public $dbms;
-    public $user;
-    public $pass;
-    public $host;
-    public $port;
-    public $dbname;
-    public $backupdir;
-    public $command;
-    public $year;
-    public $month;
-    public $day;
-    public $hour;
-    public $min;
-    public function __construct(array $info) {
-        $this->dbms = $info['dbms'];
-        $this->user = $info['user'];
-        $this->pass = $info['pass'];
-        $this->host = $info['host'];
-        $this->port = $info['port'];
-        $this->dbname = $info['dbname'];
-        $this->command = $info['command'];
-        $this->backupdir = $info['backupdir'];
-        $today = getdate();
 
-        $this->day = $today['mday'];
-        if ($this->day < 10) {
-            $this->day = "0$this->day";
-        }
-        $this->month = $today['mon'];
-        if ($this->month < 10) {
-            $this->month = "0$this->month";
-        }
-        $this->year = $today['year'];
-        $this->hour = $today['hours'];
-        $this->min = $today['minutes'];
-        $this->sec = "00";
-    }
+   public $dbms;
+   public $user;
+   public $pass;
+   public $host;
+   public $port;
+   public $dbname;
+   public $backupdir;
+   public $command;
+   public $year;
+   public $month;
+   public $day;
+   public $hour;
+   public $min;
 
-    public function createBackup($tipo=false){
-        switch($this->dbms){
-            case "MYSQL":
-                $dbHandler = new MysqlProcess;
-                break;
-            case "POSTGRESQL":
-                $dbHandler = new PostgresqlProcess;
-                break;
-            default:
-                break;
-        }
-        $resultado = $dbHandler->createSystemBackup($this);
-        return $resultado;
-    }
+   public function __construct(array $info) {
+      $this->dbms = $info['dbms'];
+      $this->user = $info['user'];
+      $this->pass = $info['pass'];
+      $this->host = $info['host'];
+      $this->port = $info['port'];
+      $this->dbname = $info['dbname'];
+      $this->command = $info['command'];
+      $this->backupdir = $info['backupdir'];
+      $today = getdate();
 
-    public function restoreBackup($fileBackup){
-        switch($this->dbms){
-            case "MYSQL":
-                $dbHandler = new MysqlProcess;
-                break;
-            case "POSTGRESQL":
-                $dbHandler = new PostgresqlProcess;
-                break;
-            default:
-                break;
-        }
-        $resultado = $dbHandler->restoreSystemBackup($this,$fileBackup);
-        return $resultado;
-    }
+      $this->day = $today['mday'];
+      if ($this->day < 10) {
+         $this->day = "0$this->day";
+      }
+      $this->month = $today['mon'];
+      if ($this->month < 10) {
+         $this->month = "0$this->month";
+      }
+      $this->year = $today['year'];
+      $this->hour = $today['hours'];
+      $this->min = $today['minutes'];
+      $this->sec = "00";
+   }
 
-    public function removeFileBackup(){
+   public function createBackup($tipo = false) {
+      switch ($this->dbms) {
+         case "MYSQL":
+            $dbHandler = new MysqlProcess;
+            break;
+         case "POSTGRESQL":
+            $dbHandler = new PostgresqlProcess;
+            break;
+         default:
+            break;
+      }
+      $resultado = $dbHandler->createSystemBackup($this);
+      return $resultado;
+   }
 
-    }
+   public function restoreBackup($fileBackup) {
+      switch ($this->dbms) {
+         case "MYSQL":
+            $dbHandler = new MysqlProcess;
+            break;
+         case "POSTGRESQL":
+            $dbHandler = new PostgresqlProcess;
+            break;
+         default:
+            break;
+      }
+      $resultado = $dbHandler->restoreSystemBackup($this, $fileBackup);
+      return $resultado;
+   }
+
+   public function removeFileBackup() {
+      
+   }
+
 }
